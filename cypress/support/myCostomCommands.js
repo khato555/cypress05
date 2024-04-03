0d68301c568546fd733f4ea797a0e74c926698bc
khato555 <162362613+khato555@users.noreply.github.com>
2024-04-03 11:52:19 +0400

    add file

--- cypress/support/myCostomCommands.js
+++ cypress/support/myCostomCommands.js
@@ -1,23 +1,22 @@
 Cypress.Commands.add("login",(Email, Password) => {
     cy.visit("https://www.cavea.ge/");
     cy.get(".col-xs-8 > .form-group > .btn-white").click();
-    cy.pause()
+    cy.wait(1000)
     cy.get("#username").type( Email);
     cy.get(".bs-login-form > .modal-dialog > .modal-content > .modal-body > form > :nth-child(5) > .form-control").type(Password);
     cy.get('form > :nth-child(6) > .btn').click()
-    /*cy.get(".login > #username").type("x.dzidzaria85@gmail.com");
-    cy.get(".login > #password").type("zarikuna555");
-    cy.get('.login > [type="submit"]').click();*/
+    
     
   });
   Cypress.Commands.add("isloggetin",()=>{
     cy.get('label').should('exist')
   })
     
-//მე-2 ტესტ ქეისი"რეგისტრაცია
-/*Cypress.Commands.add("Register",(first_name,last_name,mobile,user_email,password,confirm_password) => {
+//მე-2 ტესტ ქეისი"რეგისტრაცია"
+Cypress.Commands.add("Register",(first_name,last_name,mobile,user_email,password,confirm_password) => {
     cy.visit("https://www.cavea.ge/");
     cy.get('.col-xs-8 > .form-group > .btn-red').click()
+    cy.wait(1000)
     cy.get('#first_name').type(first_name)
     cy.get('#last_name').type(last_name)
     cy.get('#user_email').type(user_email)
@@ -30,5 +29,15 @@ Cypress.Commands.add("login",(Email, Password) => {
     cy.get(':nth-child(16) > .btn').click()
     cy.get('.bs-register-form > .modal-dialog > .modal-content > .modal-footer > .text-primary').click()
     
-});*/
-  
+});
+//მე-3 ტესტ ქეისი "არასწორად დალოგინებისას"
+Cypress.Commands.add("falslogin",(username, password) => {
+    cy.visit("https://www.cavea.ge/");
+    cy.get(".col-xs-8 > .form-group > .btn-white").click();
+    cy.get("#username").type( username0);
+    cy.get(".bs-login-form > .modal-dialog > .modal-content > .modal-body > form > :nth-child(5) > .form-control").type(Password);
+    cy.get('form > :nth-child(6) > .btn').click()
+    cy.get(".login > #username").type(username);
+    cy.get(".login > #password").type(password);
+    cy.get('.login > [type="submit"]').click();
+})
